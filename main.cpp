cc982ac69dad44d7843c216dd1e0beaf0c565d23
ThibaultGH <thibault.cimic@inria.fr>
2022-10-06 13:36:14 +0200

    Succesfully added the LAPACK library, proof by example using dgeev_.

--- main.cpp
+++ main.cpp
@@ -1,40 +1,105 @@
 #include <iostream>
+#include <fstream>
 #include <gsl/gsl_cblas.h>
 
 using namespace std;
 
-void cblas_sscal(const int N, const float alpha, float *x, const int incx);
-   
+// An example of how to declare a BLAS function in this project
+// void cblas_sscal(const int N, const float alpha, float *x, const int incx);
+
+// An example of how to declare a LAPACK function in this project
+extern "C" {
+    extern int dgeev_(char*,char*,int*,double*,int*,double*, double*, double*, int*, double*, int*, double*, int*, int*);
+    }
+
+
+
+
 int main(int argc, char *argv[])
 {
-  int N = 3;
-  float SA = 2.1;
-  float SX[N];
-  int INCX = 1;
 
-  for (int i1 = 0; i1 < N; ++i1) {
-    SX[i1] = i1;
+  // check for an argument
+  if (argc<2){
+    cout << "Usage: " << argv[0] << " " << " filename" << endl;
+    return -1;
   }
-
-
-  cout << "Before : " << endl;
   
-  for (int i2 = 0; i2 < N; ++i2) {
-    cout << SX[i2] << " ";
+  int n,m;
+  double *data;
+  
+  // read in a text file that contains a real matrix stored in column major format
+  // but read it into row major format
+  
+  char filename[] = "matrix.txt";
+
+  ifstream fin(filename, ios::in);
+  if (!fin.is_open()){
+    cout << "Failed to open " << argv[1] << endl;
+    return -1;
+  }
+  fin >> n >> m;  // n is the number of rows, m the number of columns
+  data = new double[n*m];
+  for (int i=0;i<n;i++){
+    for (int j=0;j<m;j++){
+      fin >> data[j*n+i];
+    }
+  }
+  if (fin.fail() || fin.eof()){
+    cout << "Error while reading " << argv[1] << endl;
+    return -1;
+  }
+  fin.close();
+  
+  // check that matrix is square
+  if (n != m){
+    cout << "Matrix is not square" <<endl;
+    return -1;
+  }
+  
+  // allocate data
+  char Nchar='N';
+  double *eigReal=new double[n];
+  double *eigImag=new double[n];
+  double *vl,*vr;
+  int one=1;
+  int lwork=6*n;
+  double *work=new double[lwork];
+  int info;
+  
+  // calculate eigenvalues using the DGEEV subroutine
+  dgeev_(&Nchar,&Nchar,&n,data,&n,eigReal,eigImag,
+	 vl,&one,vr,&one,
+	 work,&lwork,&info);
+  
+  
+  // check for errors
+  if (info!=0){
+    cout << "Error: dgeev returned error code " << info << endl;
+    return -1;
+  }
+  
+  // output eigenvalues to stdout
+  cout << "--- Eigenvalues ---" << endl;
+  for (int i=0;i<n;i++){
+    cout << "( " << eigReal[i] << " , " << eigImag[i] << " )\n";
   }
   cout << endl;
-
-
-  cblas_sscal(N,SA,SX,INCX);
-
-  cout << "After : " << endl;
   
-  for (int i3 = 0; i3 < N; ++i3) {
-    cout << SX[i3] << " ";
-  }
-  cout << endl;
+  // deallocate
+  delete [] data;
+  delete [] eigReal;
+  delete [] eigImag;
+  delete [] work;
+  
+
+
+  
+
+
   
   cout << "Hello world !" << endl;
+
+  // deallocate
   
   return 0;
 }
