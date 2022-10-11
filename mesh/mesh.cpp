cf8539134e054c276b779afdf945ddb3fdf6edb2
ThibaultGH <thibault.cimic@inria.fr>
2022-10-11 14:56:42 +0200

    Let's turn this into a proper class with its own constructor

--- mesh/mesh.cpp
+++ mesh/mesh.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+//Let's turn this into a proper class with its own constructor
+
 void read_mesh_file(int &nb_corners, int* &corners, int &nb_border_edges, int* &border_edges, int &nb_nodes, int &nb_elements, float* &nodes, int* elements, const char* filename){
   
   ifstream my_file(filename);
